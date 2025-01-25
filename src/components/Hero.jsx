b87bd0872d3920f5537fb1bb5aea4f95ffbe7386
Abdelrahman <Abdelrahmanmohamed5533@yahoo.com>
2025-01-25 21:10:01 +0200

    Add new Github icon

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -21,7 +21,7 @@ const Hero = () => {
         </div>
       </div>
       <ComputersCanvas />
-      <div className='absolute bottom-32 w-full flex justify-center items-center'>
+      <div className='absolute bottom-28 w-full flex justify-center items-center'>
         <a href='#about'>
           <div
             className='w-[35px] h-[64px]  rounded-3xl border-4 border-secondary flex
